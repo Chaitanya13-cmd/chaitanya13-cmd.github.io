3d33d524a4fd12b17069102082967c9e20260bc3
Chaitanya13-cmd <chaitanyagraphicsworld@gmail.com>
2025-12-31 18:43:55 +0530

    Update boss.js

--- boss.js
+++ boss.js
@@ -1,3 +1,5 @@
+alert("BOSS SCRIPT LOADED 🔥");
+
 let bossHP = 100;
 
 const btn = document.getElementById("bossBtn");
@@ -5,30 +7,15 @@ const hpText = document.getElementById("bossHp");
 const msg = document.getElementById("bossMsg");
 
 btn.addEventListener("click", () => {
-  if (bossHP <= 0) {
-    msg.innerText = "🏆 Boss already defeated! GG!";
-    return;
-  }
-
-  // Player attack damage (random)
-  const damage = Math.floor(Math.random() * 20) + 5;
-  bossHP -= damage;
+  bossHP -= 10;
   if (bossHP < 0) bossHP = 0;
 
   hpText.innerText = bossHP;
+  msg.innerText = "⚔️ You attacked the boss!";
 
   if (bossHP === 0) {
-    msg.innerText = "🎉 YOU WIN! Boss defeated 💥";
-    btn.innerText = "BOSS DOWN ☠️";
+    msg.innerText = "🏆 BOSS DEFEATED!";
     btn.disabled = true;
-  } else {
-    // Boss taunts back
-    const taunts = [
-      "😈 Boss: Is that all?",
-      "🐲 Boss: Weak attack!",
-      "🔥 Boss: Try harder!",
-      "💀 Boss: You’ll need more power!"
-    ];
-    msg.innerText = taunts[Math.floor(Math.random() * taunts.length)];
   }
 });
+

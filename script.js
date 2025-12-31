7f096f27b942a47afc921bfd102f02513e5d2b5f
Chaitanya13-cmd <chaitanyagraphicsworld@gmail.com>
2025-12-31 15:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let score = 0;
 
 const box = document.getElementById("game-box");
 const scoreText = document.getElementById("score");
+const clickSound = document.getElementById("clickSound");
 
 box.addEventListener("click", () => {
+  clickSound.currentTime = 0; // repeat fast
+  clickSound.play();
+
   score++;
   scoreText.innerText = score;
 

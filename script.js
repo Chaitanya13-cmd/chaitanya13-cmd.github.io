a3339435805f38e6e6ecc1eeb8c6500237a4bbdf
Chaitanya <chaitanyagraphicsworld@gmail.com>
2026-01-12 20:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,28 @@ function checkDailyStreak() {
 }
 
 checkDailyStreak();
+
+const modal = document.getElementById("gameModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalDesc = document.getElementById("modalDesc");
+const playBtn = document.getElementById("playBtn");
+
+let targetGameId = "";
+
+function openModal(title, desc, gameId) {
+  modalTitle.textContent = title;
+  modalDesc.textContent = desc;
+  targetGameId = gameId;
+
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+playBtn.onclick = () => {
+  closeModal();
+  document.getElementById(targetGameId)
+    .scrollIntoView({ behavior: "smooth" });
+};
